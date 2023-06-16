f028a2710000a27ae3f3b2ec79bc1d794d70596d
zhujhj <spacebound4444@gmail.com>
2023-06-15 23:48:03 -0400

    testing

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 // const nextConfig = {}
 
+// const nextConfig = {
+// trailingSlash: true,
+// output: 'export',
+// }
+// module.exports = nextConfig
 const nextConfig = {
-trailingSlash: true,
-output: 'export',
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
-module.exports = nextConfig
 
 module.exports = nextConfig
 module.exports = {
