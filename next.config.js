b1a16bbfda3f988b4918356162c409c28ae5b539
zhujhj <spacebound4444@gmail.com>
2023-06-28 01:17:10 -0400

    Update module exports

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
 
 module.exports = nextConfig
 module.exports = {
-    images: {
-      domains: ['example.com'], // Add your domain(s) where the images are hosted
-    },
-  };
+  images: {
+      unoptimized: true
+  }
+}
